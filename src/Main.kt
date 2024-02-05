6f1eb8c28c82997087fd2b0dfc6459ee6496e4ed
Enokd <edragon851@students.mchenry.edu>
2024-02-05 14:32:12 -0600

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var tireShopArray = arrayOf("snow tires", "summer tires", "tire chains", "oil","fuel filter")
+
+    for(item in tireShopArray){
+        println("$item")
+    }
+}
